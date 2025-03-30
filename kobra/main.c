b3856be94274daae7e40215e1bb88dcbc0754280
Lucas <lm.moreiragomes@gmail.com>
2025-03-30 20:40:38 -0300

    kobra v2

--- kobra/main.c
+++ kobra/main.c
@@ -16,7 +16,11 @@ typedef struct{
     int *bodyX;
     int *bodyY;
     int length;
+    int speed;
     int score;
+    int scoreSpeed;
+    int powerup;
+    int powerupColor;
 }Kobra;
 
 typedef struct{
@@ -34,9 +38,15 @@ void optionsMenu();
 
 
 
-
+Mix_Music *titleMusic;
+Mix_Music *gameplayMusic;
+Mix_Chunk *eatSound;
+Mix_Chunk *deathSound;
+Mix_Chunk *menuSound;
+Mix_Chunk *powerupSound;
 
 int lastMove = 0;
+int kobraSpeed;
 
 int main()
 {
@@ -50,8 +60,14 @@ int main()
         return 1;
     }
 
-    Mix_Chunk *eatSound = Mix_LoadWAV("audio/eatsound.wav");
-    Mix_Music *bgMusic = Mix_LoadMUS("audio/bgmusic.wav");
+
+    titleMusic = Mix_LoadMUS("audio/bgmusic.wav");
+    gameplayMusic = Mix_LoadMUS("audio/gameplay.wav");
+    eatSound = Mix_LoadWAV("audio/eatsound.wav");
+    deathSound = Mix_LoadWAV("audio/deathsound.wav");
+    menuSound = Mix_LoadWAV("audio/option.wav");
+    powerupSound = Mix_LoadWAV("audio/powerup.wav");
+
 
     while(true){
         initscr();
@@ -59,16 +75,17 @@ int main()
         noecho();
         curs_set(0);
 
-        Mix_PlayMusic(bgMusic, -1);
         int gamePoint = gameMenu();
         if(gamePoint == 1){
             srand(time(NULL));
 
+            Mix_PlayMusic(gameplayMusic, -1);
 
             //Game varia1bles
             int row = 30, col = 68;
             int direcao = KEY_LEFT;
             int isAlive = 1;
+
 
             //Setting first configurations to start the game
             initscr();
@@ -83,6 +100,12 @@ int main()
             //Kobra initializer
             Kobra kobra;
             kobra.length = 3;
+            kobra.score = 0;
+            kobra.speed = 200;
+            kobra.scoreSpeed = 0;
+            kobra.powerup = 0;
+            kobra.powerupColor = 0;
+
             kobra.bodyX = malloc(sizeof(int) * kobra.length);
             kobra.bodyY = malloc(sizeof(int) * kobra.length);
 
@@ -92,6 +115,7 @@ int main()
             food.X = col/2 + 8;
             mvprintw(food.Y, food.X, "*");
 
+            //attron(COLOR_PAIR(1));
             for(int i = 0; i < 3; i++){
                 kobra.bodyY[i] = row/2;
                 kobra.bodyX[i] = col/2 + i * 2;
@@ -102,15 +126,30 @@ int main()
                     mvprintw(kobra.bodyY[i], kobra.bodyX[i], "o");
                 }
             }
+            //attroff(COLOR_PAIR(1));
 
 
             //Actual game
             while(isAlive){
-                    moveKobra(&kobra, &direcao);
+                    mvprintw(row + 2, col/2 - 4, "SCORE: %d", kobra.score);
+                    if(!kobra.powerup){
+                        //attron(COLOR_PAIR(1));
+                        moveKobra(&kobra, &direcao);
+                        //attroff(COLOR_PAIR(1));
+                    }
+                    else{
+                        //attron(COLOR_PAIR(2));
+                        moveKobra(&kobra, &direcao);
+                        if(kobra.powerupColor == kobra.length / 3){
+                            kobra.powerup = 0;
+                            kobra.powerupColor = 0;
+                        }
+                    }
 
                 if(kobra.bodyX[0] == food.X && kobra.bodyY[0] == food.Y){
                     kobra.length += 1;
                     kobra.score += 10;
+                    kobra.scoreSpeed += 10;
                     kobra.bodyX = realloc(kobra.bodyX, sizeof(int) * kobra.length);
                     kobra.bodyY = realloc(kobra.bodyY, sizeof(int) * kobra.length);
                     spawnFood(&food, col, row);
@@ -124,13 +163,17 @@ int main()
                         nodelay(stdscr, FALSE);
                         isAlive = 0;
 
-                        mvprintw(row/2, col/2, "[GAME OVER]\nPress ENTER to exit.");
+                        mvprintw(row + 4, col/2 - 10, "[ G A M E  O V E R ]");
+                        mvprintw(row + 6, col/2 - 12, "Press ANY KEY to continue...");
+                        Mix_PauseMusic();
+                        Mix_PlayChannel(-1, deathSound, 0);
                         char userInput = getch();
-                        if(userInput == '\n'){
-                            clear();
-                            endwin();
 
-                        }
+                        clear();
+                        endwin();
+
+
+
                     }
 
                     for(int i = 1; i < kobra.length; i++){
@@ -139,13 +182,15 @@ int main()
                             nodelay(stdscr, FALSE);
                             isAlive = 0;
 
-                            mvprintw(row/2, col/2, "[GAME OVER]\nPress ENTER to exit.");
-                            char userInput = getch();
-                            if(userInput == '\n'){
-                                clear();
-                                endwin();
+                            mvprintw(row + 4, col/2 - 10, "[ G A M E  O V E R ]");
+                            mvprintw(row + 6, col/2 - 12, "Press ANY KEY to continue...");
+                            Mix_PauseMusic();
+                            Mix_PlayChannel(-1, deathSound, 0);
 
-                            }
+                            char userInput = getch();
+                            clear();
+                            endwin();
+
                         }
 
                     }
@@ -180,16 +225,27 @@ void map(int row, int col){
     for(int i = 1; i < col - 1; i+=2){
         mvprintw(0, i, "-");
         mvprintw(row - 1, i, "-");
+        mvprintw(row + 8, i, "-");
     }
 
     //Write vertical border of map
-    for(int i = 1; i < row - 1; i++){
+    for(int i = 1; i < row + 8; i++){
         mvprintw(i, 0, "|");
-        mvprintw(i, col -1, "|");
+        mvprintw(i, col - 1, "|");
     }
+
+
 }
 
 void moveKobra(Kobra *kobra, int *direction){
+
+    start_color();
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
+    init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+
+
+    attron(COLOR_PAIR(1));
+
     int move = getch();
 
     if(move != ERR){
@@ -203,7 +259,7 @@ void moveKobra(Kobra *kobra, int *direction){
     }
 
     if(*direction == KEY_LEFT){
-        lastMove = KEY_LEFT;
+
 
         mvprintw(kobra->bodyY[kobra->length - 1], kobra->bodyX[kobra->length - 1], " ");
 
@@ -225,7 +281,7 @@ void moveKobra(Kobra *kobra, int *direction){
     }
 
     if(*direction == KEY_RIGHT){
-        lastMove = KEY_RIGHT;
+
 
         mvprintw(kobra->bodyY[kobra->length - 1], kobra->bodyX[kobra->length - 1], " ");
 
@@ -247,7 +303,7 @@ void moveKobra(Kobra *kobra, int *direction){
     }
 
     if(*direction == KEY_UP){
-        lastMove = KEY_UP;
+
 
         mvprintw(kobra->bodyY[kobra->length - 1], kobra->bodyX[kobra->length - 1], " ");
 
@@ -269,7 +325,7 @@ void moveKobra(Kobra *kobra, int *direction){
     }
 
     if(*direction == KEY_DOWN){
-        lastMove = KEY_DOWN;
+
         mvprintw(kobra->bodyY[kobra->length - 1], kobra->bodyX[kobra->length - 1], " ");
 
         for(int i = kobra->length - 1; i > 0; i--){
@@ -287,12 +343,35 @@ void moveKobra(Kobra *kobra, int *direction){
             }
         }
     }
+    attroff(COLOR_PAIR(1));
 
+    if(kobra->powerup && kobra->powerupColor < kobra->length){
+        attron(COLOR_PAIR(2));
+        for(int i = 0; i == 0 || i < kobra->length; i++){
+            if(i == 0){
+                mvprintw(kobra->bodyY[i], kobra->bodyX[i], "@");
+            }
+            else{
+                mvprintw(kobra->bodyY[i], kobra->bodyX[i], "o");
+            }
+        }
+        kobra->powerupColor+=1;
+        attroff(COLOR_PAIR(2));
+    }
 
 
     flushinp();
-    napms(200);
 
+
+
+    if(kobra->scoreSpeed != 0 && kobra->scoreSpeed % 100 == 0){
+        kobra->powerup = 1;
+        kobra->speed -= 20;
+        kobra->scoreSpeed = 0;
+        Mix_PlayChannel(-1, powerupSound, 0);
+    }
+    //mvprintw(row/2, col/2, "%d", kobra->score);
+    napms(kobra->speed);
 }
 
 void spawnFood(Food *food, int col, int row){
@@ -302,6 +381,10 @@ void spawnFood(Food *food, int col, int row){
 }
 
 int gameMenu(){
+    if(!Mix_PlayingMusic() || Mix_PausedMusic()){
+        Mix_PlayMusic(titleMusic, -1);
+    }
+
 
     int menuRow = 40, menuCol = 68;
     int optionPicked = 1;
@@ -322,16 +405,26 @@ int gameMenu(){
             mvprintw(i, menuCol -1, "|");
         }
 
-        mvprintw(menuRow/2 - 2, menuCol/2 - 4, "K O B R A");
+        //KOBRA TITLE
+        mvprintw(menuRow/2 - 10, menuCol/2 - 20,  " _  __   ___    ____    ____       _");
+        mvprintw(menuRow/2 - 9, menuCol/2 - 20,  "| |/ /  / _ \\  | __ )  |  _ \\     / \\");
+        mvprintw(menuRow/2 - 8, menuCol/2 - 20,  "| ' /  | | | | |  _ \\  | |_) |   / _ \\  ");
+        mvprintw(menuRow/2 - 7, menuCol/2 - 20,  "| . \\  | |_| | | |_) | |  _ <   / ___ \\ ");
+        mvprintw(menuRow/2 - 6, menuCol/2 - 20,  "|_|\\_\\  \\___/  |____/  |_| \\_\\ /_/   \\_\\");
 
-        char options[3][13] = {"Play", "Instructions", "Exit"};
+
+
+
+
+
+        char options[3][13] = {"Play", "Instructions", "Quit"};
         for(int i = 0; i < 3; i++){
-            mvprintw(menuRow/2 + ((i + 1) * 2), menuCol/2 - 4, "%s", options[i]);
+            mvprintw(menuRow/2 + ((i + 1) * 2), menuCol/2 - 6, "%s", options[i]);
         }
 
         if(optionPicked == 1 || optionPicked == 4){
-            mvprintw(menuRow/2 + 6, menuCol/2 - 6, " ");
-            mvprintw((menuRow/2 + 2), menuCol/2 - 6, ">");
+            mvprintw(menuRow/2 + 6, menuCol/2 - 8, " ");
+            mvprintw((menuRow/2 + 2), menuCol/2 - 8, ">");
             optionPicked = 1;
         }
 
@@ -341,16 +434,19 @@ int gameMenu(){
         //Navegating through menu options
         if(userInput == KEY_DOWN && optionPicked != 3){
             optionPicked+=1;
-            mvprintw(optionPosition, menuCol/2 - 6, " ");
+            mvprintw(optionPosition, menuCol/2 - 8, " ");
             optionPosition+=2;
-            mvprintw(optionPosition, menuCol/2 - 6, ">");
+            mvprintw(optionPosition, menuCol/2 - 8, ">");
+            Mix_PlayChannel(-1, menuSound, 0);
         }
         if(userInput == KEY_UP && optionPicked != 1){
             optionPicked-=1;
-            mvprintw(optionPosition, menuCol/2 - 6, " ");
+            mvprintw(optionPosition, menuCol/2 - 8, " ");
             optionPosition-=2;
-            mvprintw(optionPosition, menuCol/2 - 6, ">");
+            mvprintw(optionPosition, menuCol/2 - 8, ">");
+            Mix_PlayChannel(-1, menuSound, 0);
         }
+
 
         //When an option is selected, returns an integer
 
@@ -381,7 +477,7 @@ void optionsMenu(){
             mvprintw(i, optionCol -1, "|");
         }
 
-    char instructions[4][60] = {"Use ARROWS to control Kobra", "You must feed Kobra by eating the food (*)", "If Kobra hits any place with its head, its over!", "Press [ENTER] to continue..."};
+    char instructions[4][60] = {"Use ARROWS to control Kobra", "You must feed Kobra by eating the food (*)", "If Kobra hits any place with its head, its over!", "Press ANY KEY to continue..."};
     mvprintw(optionRow/2 - 12, optionCol/2 - 6, "INSTRUCTIONS");
     for(int i = 0; i < 4; i++){
         mvprintw(optionRow/2 + (i * 1), optionCol/2 - 20, "%s", instructions[i]);
@@ -389,9 +485,8 @@ void optionsMenu(){
     }
 
     int userInput = getch();
-    if(userInput == 10){
-        clear();
-    }
+    clear();
+
 }
 
 void gameSound(){
